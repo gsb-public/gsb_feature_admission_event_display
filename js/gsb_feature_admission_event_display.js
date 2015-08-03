4410a13548f0c1a916e205672cb91c19b2c28804
Ayuta Padhi <apadhi@stanford.edu>
2015-08-03 14:22:23 -0700

    WP-5779 Date filter and search fix for admission event. This includes WP-5775 and WP-5784

--- js/gsb_feature_admission_event_display.js
+++ js/gsb_feature_admission_event_display.js
@@ -2,12 +2,15 @@
 
   Drupal.behaviors.gsb_feature_admission_event_display = {
     attach: function (context, settings) {
-      var height = $("div.views-exposed-form").height();
-      var width = $("div.views-exposed-form").width();
-      $('div.views-exposed-form :input').change(function(e) {
+
+      var height = $("div.views-exposed-widgets").height();
+      var width = $("div.views-exposed-widgets").width();
+      $('div.container-inline-date :input').change(function(e) {
         overlay_div = '<div id="let-ajax-alone" style="height:' + height + 'px;width:' + width + 'px; position:absolute; margin-bottom:-' + height + 'px;margin-right:-' + width + 'px;z-index:999;"></div>';
-        $('div.views-exposed-form').prepend(overlay_div);
+        $('div.views-exposed-widgets').prepend(overlay_div);
       });
+      
+
     }
 };
 
