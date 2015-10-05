7af234560b3879ae5a689a12a3bff76f2d0192d6
Ayuta Padhi <apadhi@stanford.edu>
2015-10-05 09:24:23 -0700

    WP-5994 WP-5815 Fix for missing multiday event with past start date and added code not to allow empty dates filter

--- js/gsb_feature_admission_event_display.js
+++ js/gsb_feature_admission_event_display.js
@@ -2,15 +2,26 @@
 
   Drupal.behaviors.gsb_feature_admission_event_display = {
     attach: function (context, settings) {
-
+      if ($("#edit-date-search-value-datepicker-popup-0").val() == "" ){
+        var start_date = $.datepicker.formatDate('M dd yy', new Date());
+        $("#edit-date-search-value-datepicker-popup-0").val(start_date);
+        $('#ui-datepicker-div').hide();
+        $("#edit-date-search-value-datepicker-popup-0").change();
+      }
+      if ($("#edit-field-event-date-value-value-datepicker-popup-0").val() == ""){
+        var date = new Date();
+        var end_date=(date.getMonth()+1) + "/" + date.getDate() + "/" + (date.getFullYear() + 1);
+        $("#edit-field-event-date-value-value-datepicker-popup-0").val($.datepicker.formatDate('M dd yy', new Date(end_date)));
+        $('#ui-datepicker-div').hide();
+        $("#edit-field-event-date-value-value-datepicker-popup-0").change();
+      }
       var height = $("div.views-exposed-widgets").height();
       var width = $("div.views-exposed-widgets").width();
       $('div.container-inline-date :input').change(function(e) {
+
         overlay_div = '<div id="let-ajax-alone" style="height:' + height + 'px;width:' + width + 'px; position:absolute; margin-bottom:-' + height + 'px;margin-right:-' + width + 'px;z-index:999;"></div>';
         $('div.views-exposed-widgets').prepend(overlay_div);
       });
-      
-
     }
 };
 
