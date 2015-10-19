5305e045741998c17f4787f331c1b53c5891f6e8
Meghan Palagyi <meghanp@stanford.edu>
2015-10-19 15:58:08 -0700

    WP-6109: update location display.

--- js/gsb_feature_admission_event_location.js
+++ js/gsb_feature_admission_event_location.js
@@ -2,14 +2,11 @@
 
   Drupal.behaviors.gsb_feature_admission_event_location = {
     attach: function (context, settings) {
-      //$('.view-content h3').each(function(){
-      //  $(this).nextUntil('h3')
-      //    .wrapAll('<div class="accordion-body-wrap">').parent().add(this);
-      //});
+
       $('.view-content h3').each(function() {
         $(this).addClass('acc-title');
       });
-      //$('.accordion-body-wrap div').addClass('acc-body');
+
       $('.view-content div').addClass('acc-body');
     }
   };
